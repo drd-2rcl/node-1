7515998103c3c2c0ee486664fe25e0a7b0e5224e
drd-2rcl <drd.rclrd@gmail.com>
2018-06-26 14:41:02 -0300

    finish project

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,30 +1,9 @@
 module.exports = function(application) {
   application.get('/formulario_inclusao_noticias', function(req, res){
-    res.render("admin/form_add_noticias");
+    application.app.controllers.admin.formulario_inclusao_noticia(application, req, res);
   });
+
   application.post('/noticias/salvar', function(req, res){
-    var noticia = req.body;
-
-    req.assert('título', 'Título é obrigatório').notEmpty();
-    req.assert('resumo', 'Resumo é obrigatório').notEmpty();
-    req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
-    req.assert('data_noticia', 'Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'});
-    req.assert('noticia', 'Notícia é obrigatório').notEmpty();
-
-    var erros = req.validationErrors();
-
-    if(erros){
-      res.render("admin/form_add_noticia");
-      return;
-    }
-
-    var connection = application.config.dbConnection();
-    var noticiasModel = new application.app.models.NotciasDAO(connection);
-
-    noticiasModel.salvarNoticia(noticia, function(error, result){
-      res.redirect('/noticias');
-    });
-
-
+    application.app.controlers.admin.noticias_salvar(application, req, res);
   });
 }
